b181df6e2a433b6486002c654b3ef64b0f2a1f88
Josue <Josue@192.168.1.2>
2017-01-10 08:44:24 -0600

    D.E.L.T.A-1.PROJEC

---
+++ src/main/java/una/model/Pacientes.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package una.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Josue
+ */
+@Entity
+@Table(name = "pacientes")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pacientes.findAll", query = "SELECT p FROM Pacientes p")
+    , @NamedQuery(name = "Pacientes.findByNombre", query = "SELECT p FROM Pacientes p WHERE p.nombre = :nombre")
+    , @NamedQuery(name = "Pacientes.findByTelefono", query = "SELECT p FROM Pacientes p WHERE p.telefono = :telefono")
+    , @NamedQuery(name = "Pacientes.findByDireccion", query = "SELECT p FROM Pacientes p WHERE p.direccion = :direccion")
+    , @NamedQuery(name = "Pacientes.findByFechaNacimiento", query = "SELECT p FROM Pacientes p WHERE p.fechaNacimiento = :fechaNacimiento")
+    , @NamedQuery(name = "Pacientes.findByEnfermedades", query = "SELECT p FROM Pacientes p WHERE p.enfermedades = :enfermedades")
+    , @NamedQuery(name = "Pacientes.findByObservacion", query = "SELECT p FROM Pacientes p WHERE p.observacion = :observacion")
+    , @NamedQuery(name = "Pacientes.findById", query = "SELECT p FROM Pacientes p WHERE p.id = :id")})
+public class Pacientes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "telefono")
+    private Integer telefono;
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "fechaNacimiento")
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+    @Column(name = "enfermedades")
+    private String enfermedades;
+    @Column(name = "observacion")
+    private String observacion;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @OneToMany(mappedBy = "idPaciente")
+    private Collection<Citas> citasCollection;
+
+    List<Pacientes> pacientes;
+
+    public Pacientes() {
+        this.id = id;
+        this.nombre = "";
+        this.telefono = 0;
+        this.direccion = "";
+        this.enfermedades = "";
+        this.observacion = "";
+        this.pacientes = new ArrayList<Pacientes>() ;
+
+    }
+
+    public Pacientes(Integer id, String nombre, Integer telefono, String direccion, String enfermedades, String observaciones, List<Pacientes> pacientes) {
+        this.id = id;
+        this.nombre = "";
+        this.telefono = 0;
+        this.direccion = "";
+        this.enfermedades = "";
+        this.observacion = "";
+        this.pacientes = pacientes ;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEnfermedades() {
+        return enfermedades;
+    }
+
+    public void setEnfermedades(String enfermedades) {
+        this.enfermedades = enfermedades;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @XmlTransient
+    public Collection<Citas> getCitasCollection() {
+        return citasCollection;
+    }
+
+    public void setCitasCollection(Collection<Citas> citasCollection) {
+        this.citasCollection = citasCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pacientes)) {
+            return false;
+        }
+        Pacientes other = (Pacientes) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<Pacientes> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(List<Pacientes> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.Pacientes[ id=" + id + " ]";
+    }
+
+}
